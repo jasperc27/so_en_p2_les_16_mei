186b5f7b466870ea54e403ec0fc7b52cafbe4009
JohannesDeSmedt <johannes.desmedt@kuleuven.be>
2025-05-16 15:12:31 +0200

    Update rector_verkiezing.py

--- rector_verkiezing.py
+++ rector_verkiezing.py
@@ -7,3 +7,12 @@ class RectorKandidaat(Kandidaat):
 
     def __str__(self):
         return f"{self.naam} (Rector: {self.faculteit})"
+    
+class RectorStem(Stem):
+    def __init__(self, kandidaat, faculteit):
+        super().__init__(kandidaat)
+        self.faculteit = faculteit
+
+    def __str__(self):
+        return f"Stem op {self.kandidaat} (Rector: {self.faculteit})"
+    
